112318e85d1fc9b0b07d4b9522b318489fd68f64
Adrian Vollmer <adrian.vollmer@syss.de>
2017-03-10 08:57:21 +0100

    removed readme from script; added support for comments in hosts file

--- dns-mitm.py
+++ dns-mitm.py
@@ -3,29 +3,6 @@
 
 """
 Usage: dns-mitm.py -h
-
-This is a fake DNS server that answers requests for a domain's A record with
-a custom IP address. It is intended to be used in an isolated network for
-pentests.
-
-If you want to put yourself in a MitM position of a given connection, you
-would usually do so by modifying the network setup: either in software
-(ARP-spoofing etc.) or in hardware (unplugging network cables). This script
-is for situations in which you are unable or unwilling to change the network
-setup but have control over the "victim" device.
-
-Possible use cases could be:
- * You want to analyze traffic of a mobile app you are testing, so you
- change the DNS server on your mobile device
- * You want to filter ads on your TV, so you set its DNS server to your
- Raspberry Pi on the same network which is running this script
-
-It makes sense to assign multiple IP addresses to your device, for example
-with `ip address add 192.168.1.16/28 dev eth0`.
-
-This way you can spoof multiple domains with an indiviual IP address.
-Otherwise, you won't know the original destination of the intercepted
-traffic arriving at your machine without deep package inspection.
 """
 
 
@@ -71,6 +48,7 @@ def fill_hosts_list():
         with open(args.HOSTS_FILE, 'r') as f:
             hosts = f.readlines()
         for line in hosts:
+            line = re.sub(r'#.*$', '', line)
             line = re.sub(r'\s+', ' ', line.strip())
             HOSTS_LIST += [line.split(' ')]
 
